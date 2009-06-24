124e8ce931a6c76af5fee9f12842a40812efcd7b
Richard Eckart de Castilho <richard.eckart@gmail.com>
2009-06-24 13:24:27 +0000

    Per default search env TREETAGGER_HOME, TAGDIR and sysprop treetagger.home for models and executables.

--- org.annolab.tt4j/src/main/java/org/annolab/tt4j/DefaultModelResolver.java
+++ org.annolab.tt4j/src/main/java/org/annolab/tt4j/DefaultModelResolver.java
@@ -7,27 +7,45 @@ import java.io.IOException;
  * Simple model provider. The model name is actually the path to the model.
  * The path has to be followed by a ":" and the name model encoding. Example
  * <code>/usr/lib/model.par:UTF-8</code>.
- * 
+ *
  * @author Richard Eckart
  */
-public 
-class DefaultModelResolver 
-implements ModelResolver 
+public
+class DefaultModelResolver
+implements ModelResolver
 {
-	public 
+	public
 	Model getModel(
-			final String aModelName) 
-	throws IOException 
+			final String aModelName)
+	throws IOException
 	{
 		return new Model() {
 			private final String _encoding;
-			private final File _file;
+			private File _file;
 			private final String _name;
 			{
 				_name = aModelName;
 				String[] fields = aModelName.split(":");
+
+				// The using the name as path
 				_file = new File(fields[0]);
-				
+
+				// Try using the system property
+				if (!_file.exists() && (System.getProperty("treetagger.home") != null)) {
+					_file = new File(System.getProperty("treetagger.home")+"/models/"+fields[0]);
+				}
+
+				// Try using the environment variable from the python wrapper
+				if (!_file.exists() && (System.getenv("TAGDIR") != null)) {
+					_file = new File (System.getenv("TAGDIR")+"/models/"+fields[0]);
+				}
+
+				// Fall back to the file itself which will cause an exception
+				// later.
+				if (!_file.exists()) {
+					throw new IOException("Unable to locate model ["+fields[0]+"]");
+				}
+
 				if (fields.length > 1) {
 					_encoding = fields[1];
 				}
@@ -35,49 +53,49 @@ implements ModelResolver
 					_encoding = "UTF-8";
 				}
 			}
-			
-			public 
-			String getName() 
+
+			public
+			String getName()
 			{
 				return _name;
 			}
-			
-			public 
-			void destroy() 
+
+			public
+			void destroy()
 			{
 				// Do nothing
 			}
 
-			public 
-			String getEncoding() 
+			public
+			String getEncoding()
 			{
 				return _encoding;
 			}
 
-			public 
+			public
 			File getFile() {
 				return _file;
 			}
 
-			public 
-			String getFlushSequence() 
+			public
+			String getFlushSequence()
 			{
 				return ".\n.\n.\n.\n";
 			}
 
-			public 
-			void install() 
-			throws IOException 
+			public
+			void install()
+			throws IOException
 			{
 				// Do nothing.
 			}
-			
+
 		};
 	}
 
-	public 
+	public
 	void setPlatformDetector(
-			PlatformDetector aPlatform) 
+			PlatformDetector aPlatform)
 	{
 		// Do nothing
 	}
