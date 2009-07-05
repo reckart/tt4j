304cf04f0b1d164db8f90b5dc523d85fe81d465c
eckart <eckart@eae9d0b1-32c4-a60b-f2c5-3a7372d819d4>
2009-07-05 08:47:02 +0000

    [maven-release-plugin]  copy for tag org.annolab.tt4j-1.0.0

---
+++ tt4j/tags/org.annolab.tt4j-1.0.0/src/main/java/org/annolab/tt4j/ExecutableResolver.java
@@ -0,0 +1,15 @@
+package org.annolab.tt4j;
+
+import java.io.IOException;
+
+public
+interface ExecutableResolver
+{
+	void setPlatformDetector(
+			PlatformDetector aPlatform);
+
+	void destroy();
+
+	String getExecutable()
+	throws IOException ;
+}
---
+++ tt4j/tags/org.annolab.tt4j-1.0.0/src/main/java/org/annolab/tt4j/UnsupportedPlatformException.java
@@ -0,0 +1,15 @@
+package org.annolab.tt4j;
+
+public
+class UnsupportedPlatformException
+extends Exception
+{
+	private static final long serialVersionUID = 2645508230245164052L;
+
+	public
+	UnsupportedPlatformException(
+			final String aMessage)
+	{
+		super(aMessage);
+	}
+}
